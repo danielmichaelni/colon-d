9b34a9635f28f569548b8b8f7ddc85404ea7d42c
Daniel Ni <danielmichaelni@gmail.com>
2026-05-15 22:41:48 -0400

    Initial Commit

---
+++ ColonD/ColonDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColonDApp.swift
+//  ColonD
+//
+//  Created by Daniel Ni on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ColonDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColonD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColonD
+//
+//  Created by Daniel Ni on 5/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
